775bdae3791d7ee7940a82440f59dcea950f94e7
Michael Babiy <iC@Michaels-MacBook-Pro.local>
2014-06-03 09:53:16 -0700

    Initial commit.

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var images = String[]()
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        images = ["220.jpg"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int
+    {
+        return images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell!
+    {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as HomeCollectionViewCell
+        cell.imageView.image = UIImage(named:images[indexPath.row])
+        return cell
+    }
+    
 }
 
